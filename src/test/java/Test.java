32a9461f2ebfd58c741675cdfa0bb93a894b174c
Benjamin Kissinger <benjamin@kissinger.at>
2023-10-23 14:24:21 +0200

    Tests

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Test {
     @org.junit.jupiter.api.Test
-    public void test() {
+    public void test1() {
         Rechtschreibtrainer t1 = new Rechtschreibtrainer();
         t1.addWort("https://api.ardmediathek.de/image-service/images/urn:ard:image:3dab66faa8140d8b?w=448&ch=9e935de585dfb889", "katze");
         t1.getRandomWort();
